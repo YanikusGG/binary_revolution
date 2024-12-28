b2a3caca1941b884e7d5c746d53c5ba3da3d87cf
Anikiev Yan <yanikusgg@yandex.ru>
2024-12-28 08:10:40 +0300

    First version

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 5f;
+    public float jumpForce = 10f;
+
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        Vector2 movement = new Vector2(moveHorizontal * speed * Time.deltaTime, rb.velocity.y);
+        rb.velocity = movement;
+    }
+}
